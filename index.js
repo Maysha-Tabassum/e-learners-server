71d895fb36ec9599eba4844f30f02e1af08e34b4
Maysha-Tabassum <mayshatabassum77@gmail.com>
2022-10-27 18:08:27 +0600

    category api added

--- index.js
+++ index.js
@@ -5,22 +5,33 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const courses = require('./data/courses.json');
-const course_info = require('./data/course-info.json');
+const categories = require('./data/categories.json');
+const course = require('./data/course.json')
+
 
 app.get('/', (req, res) => {
     res.send('Courses API running');
 });
 
-app.get('/courses', (req, res) => {
-    res.send(courses);
+app.get('/course-categories', (req, res) => {
+    res.send(categories);
 })
 
-app.get('/course_info/:id', (req, res) => {
+app.get('/course/:id', (req, res) => {
     const id = req.params.id;
-    const selectedCourse = course_info.find(c => c._id === id);
+    const selectedCourse = course.find(c  => c._id === id);
     res.send(selectedCourse);
 })
+
+app.get('/category/:id', (req,res) => {
+    const id = req.params.id;
+    const category_course = course.filter(c => c.category_id === id);
+    res.send(category_course);
+})
+
+
+
+
 
 app.listen(port, () => {
     console.log('e-learners server runing:', port);
